a6f4fc75988cad5cc233d884ad84033f6f900982
superzbo <zbo2@qq.com>
2015-03-12 22:12:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/twer/hellocalculator/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.twer.hellocalculator;
+
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    EditText vFirstValue;
+    EditText vSecondValue;
+    TextView vResult;
+    Button vCalculate;
+    float vNum1,vNum2,vOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vFirstValue=(EditText)findViewById(R.id.EditText01);
+        vFirstValue.setText("0");
+        vFirstValue.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                vFirstValue.setText("");
+                return false;
+            }
+        });
+        vSecondValue=(EditText)findViewById(R.id.EditText02);
+        vSecondValue.setText("0");
+        vSecondValue.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                vSecondValue.setText("0");
+                return false;
+            }
+        });
+        vResult=(TextView)findViewById(R.id.TextView01);
+        vResult.setText("0.00");
+        vCalculate=(Button)findViewById(R.id.Button01);
+        vCalculate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showResult(vFirstValue.getText().toString(),vSecondValue.getText().toString());
+            }
+        });
+    }
+
+    private void showResult(String s1, String s2) {
+        vNum1=Float.parseFloat(s1);
+        vNum2=Float.parseFloat(s2);
+        vOutput=vNum1*vNum2;
+        vResult.setText(String.valueOf(vOutput));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
